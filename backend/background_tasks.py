de145fc19f6cf6c789bcc321dc1451c39c6d1ed3
Gaji Yaqub <gajiyakub6@gmail.com>
2026-02-18 10:00:00 +0100

    Working on background tasks

--- backend/background_tasks.py
+++ backend/background_tasks.py
@@ -2,7 +2,7 @@ from fastapi import BackgroundTasks, FastAPI
 
 app = FastAPI()
 
-def write_notification(email: str, message=""): # This is a write function opening a file and writing to it.
+def write_notification(email: str, message=""): # This is a write function opening a file and writing to it, simulating a notification being sent to the user. In a real world application, this could be an email or a push notification.
   with open("log.txt", mode="w") as email_file:
     content = f"Notification for {email}: {message}"
     email_file.write(content)
